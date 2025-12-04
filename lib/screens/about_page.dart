9e5051c23bd6821a79b1730173dfce051ae74fbd
Maheer Basit <maheerbasitahmed@gmail.com>
2025-12-04 19:41:17 +0000

    Fixing about us main header layout

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -9,71 +9,63 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        automaticallyImplyLeading: false,
-      ),
-      body: Column(
-        children: [
-          MainHeader(
-            currentRoute: '/about',
-            onNavigate: (route) => Navigator.pushNamed(context, route),
-            onSaleTap: () => Navigator.pushNamed(context, '/sale'),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 12.0),
-            child: Center(
-              child: Text(
-                'About us',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            MainHeader(
+              currentRoute: '/about',
+              onNavigate: (route) => Navigator.pushNamed(context, route),
+              onSaleTap: () => Navigator.pushNamed(context, '/sale'),
             ),
-          ),
-          const Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(20, 40, 32, 24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Welcome to the Union Shop!',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    SizedBox(height: 12),
-                    Text(
-                      'We are dedicated to providing quality merchandise for our community.',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'We hope you enjoy our products as much as we enjoy offering them to you.',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Happy shopping!',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      '- The Union Shop Team',
-                      textAlign: TextAlign.left,
-                      style:
-                          TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
-                    )
-                  ],
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 12.0),
+              child: Center(
+                child: Text(
+                  'About us',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                 ),
               ),
             ),
-          ),
-          const Footer(),
-        ],
+            const Padding(
+              padding: EdgeInsets.fromLTRB(20, 24, 32, 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome to the Union Shop!',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 12),
+                  Text(
+                    'We are dedicated to providing quality merchandise for our community.',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'We hope you enjoy our products as much as we enjoy offering them to you.',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Happy shopping!',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    '- The Union Shop Team',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 18, fontStyle: FontStyle.italic),
+                  )
+                ],
+              ),
+            ),
+            const Footer(),
+          ],
+        ),
       ),
     );
   }
